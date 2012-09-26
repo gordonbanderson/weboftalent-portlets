c91ef5feb77a8c15404d6ac12417b1ef9a2a1485
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-09-26 13:44:07 +0700

    ENHANCEMENT: Added where filter

--- code/RenderPortlets.php
+++ code/RenderPortlets.php
@@ -12,6 +12,7 @@ class RenderablePortlets_Controller extends Extension {
 	As Silverstripe can only deal with a single parameter string with no spaces, split the params by a pipe character.
 	These parameters are as follows:
 	* The DataObject class that we wish to show
+  * The filter to use (i.e. the where clause, leave blank to ignore)
 	* The order (descending) to show items (normally the home page is for the most recent things)
 	* The number of items maximum to show
 	* The template used to show it
@@ -21,23 +22,30 @@ class RenderablePortlets_Controller extends Extension {
 	 public function RenderPortlet($modelOrderNumberTemplate) {
         $result = null;
         $splits = explode('|', $modelOrderNumberTemplate);
-        $model = $splits[0];
-        $ordering = $splits[1];
-        $amount = $splits[2];
-        $template = $splits[3];
+        if (sizeof($splits) == 5) {
+          $model = $splits[0];
+          $where = $splits[1];
+          $ordering = $splits[2];
+          $amount = $splits[3];
+          $template = $splits[4];
 
-        error_log("TEMPLATE:".$template);
+          error_log("TEMPLATE:".$template);
 
-        $records = DataObject::get($model, '', $ordering, '', $amount);
+          $records = DataObject::get($model, $where, $ordering, '', $amount);
 
-        return $this->owner->customise(
-          new ArrayData(
-            array(
-              'Key' => 45,
-              'Records' => $records
+          return $this->owner->customise(
+            new ArrayData(
+              array(
+                'Records' => $records
+              )
             )
-          )
-        )->renderWith($template);
+          )->renderWith($template);
+        } else {
+          return 'Error with '.$modelOrderNumberTemplate;
+        }
+
+        
+        
     }
 
 }
