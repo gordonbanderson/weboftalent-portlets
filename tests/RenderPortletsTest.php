23664f95af0dea94c510d58ab90c1d154e0cdc39
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-20 00:23:24 +0700

    WIP: Initial tests shell

---
+++ tests/RenderPortletsTest.php
@@ -0,0 +1,8 @@
+<?php
+
+class RenderPortletsTest extends SapphireTest {
+	public function testRenderPortlet() {
+		$this->markTestSkipped('TODO');
+	}
+
+}
---
+++ tests/RenderableAsPortletTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class RenderableAsPortletTest extends SapphireTest {
+	public function testGetPortletTitle() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testGetPortletImage() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testGetPortletCaption() {
+		$this->markTestSkipped('TODO');
+	}
+
+}
