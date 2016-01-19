99964b041a596b0f6e55101d84c190eca8b3d067
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-20 00:28:25 +0700

    FIX: Messages are now HTML encoded

--- code/RenderPortlets.php
+++ code/RenderPortlets.php
@@ -1,37 +1,41 @@
 <?php
 
-class RenderablePortlets_Controller extends Extension {
+class RenderablePortlets_Controller extends Extension
+{
+    /*
+    Render a portlet from a template call of the form
 
-  /*
-  Render a portlet from a template call of the form
-
-  <code>
+    <code>
     $RenderPortlet(NewsItem|Title|4|Carousel)
-  </code>
+    </code>
 
-  As Silverstripe can only deal with a single parameter string with no spaces, split the params by a pipe character.
-  These parameters are as follows:
-  * @param model The DataObject class that we wish to show
-  * @param where The filter to use (i.e. the where clause, leave blank to ignore)
-  * @param ordering The order (descending) to show items (normally the home page is for the most recent things)
-  * @param amount The number of items maximum to show
-  * @param template The template used to show it
+    As Silverstripe can only deal with a single parameter string with no spaces,
+    split the params by a pipe character. These parameters are as follows:
+    * @param model The DataObject class that we wish to show
+    * @param where The filter to use (i.e. the where clause, leave blank to
+      ignore)
+    * @param ordering The order (descending) to show items (normally the home
+      page is for the most recent things)
+    * @param amount The number of items maximum to show
+    * @param template The template used to show it
 
-  FIXME: Use a join to make image extraction more efficient
-  */
-  public function RenderPortlet( $model, $where, $ordering, $amount, $template, $joinTable = '', $joinOn = '' ) {
-    $result = null;
-    //$records = DataList::DataObject::get( $model, $where, $ordering, '', $amount );
-    $records = DataList::create($model)->where($where)->sort($ordering)->limit($amount);
-    if ($joinTable) {
-      $records->leftJoin($joinTable, $joinOn);
+    FIXME: Use a join to make image extraction more efficient
+    */
+    public function RenderPortlet($model, $where, $ordering, $amount, $template,
+                                     $joinTable = '', $joinOn = '')
+    {
+        $result = null;
+        $records = DataList::create($model)->where($where)->sort($ordering)->limit($amount);
+        if ($joinTable) {
+            $records->leftJoin($joinTable, $joinOn);
+        }
+
+        return $this->owner->customise(
+            new ArrayData(
+                array(
+                'Records' => $records,
+                )
+            )
+        )->renderWith($template);
     }
-    return $this->owner->customise(
-      new ArrayData(
-        array(
-          'Records' => $records
-        )
-      )
-    )->renderWith( $template );
-  }
 }
